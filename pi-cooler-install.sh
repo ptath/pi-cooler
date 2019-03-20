9f329c739c421e09f4505b2c0df6101fbe06a17b
Арсений Шестаков <ptath@ptath.ru>
2019-03-20 22:28:15 +0400

    New installer

--- pi-cooler-install.sh
+++ pi-cooler-install.sh
@@ -6,11 +6,17 @@
 
 # Checking ~/scripts folder
 [ ! -d ~/scripts ] && mkdir ~/scripts
-[ -e ~/scripts/cooler.sh ] && echo " Script already installed (~/scripts/cooler.sh)? Exiting..." && exit
+[ -e ~/scripts/cooler.sh ] &&
+  echo " Script already installed (~/scripts/cooler.sh)?" &&
+  echo " Remove it manually (rm ~/scripts/cooler.sh) and run again to reinstall" &&
+  exit
 
 # Downloading script and cooler.service file
+echo " Downloading script to ~/scripts/cooler.sh ..."
 wget -q -N -O ~/scripts/cooler.sh https://github.com/ptath/pi-cooler/raw/master/scripts/cooler.sh
+echo " Making it executable..."
 chmod +x ~/scripts/cooler.sh
+echo " Done!"
 
 # Installing cooler.sh as systemd service
 read -n 1 -p " Install script as systemd service and reboot? (Y/n): " choice
@@ -23,9 +29,13 @@ case $choice in
     sudo chmod 644 /lib/systemd/system/cooler.service
     sudo systemctl daemon-reload
     sudo systemctl enable cooler.service
+    echo " Will reboot in 5 sec, press CTRL+C to abort..."
+    sleep 5
     sudo reboot now
     ;;
   n|N|* )
-    echo " No" && exit
+    echo " No"
+    echo " You can run script manually (sudo ~/scripts/cooler.sh)"
+    exit
     ;;
 esac
