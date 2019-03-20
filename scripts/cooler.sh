09d5762ade62a74b4f22056be8c2b26181f8c69e
Арсений Шестаков <ptath@ptath.ru>
2019-03-20 22:29:43 +0400

    Less debug messages, removed sudo from script body

--- scripts/cooler.sh
+++ scripts/cooler.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# Version 0.3
+# Version 0.4
 # By ptath (https://ptath.ru)
 # Raspberry Pi cooler script
 # Edit values below to fit your needs
 
 GPIO_N="18" # Set GPIO used to control fan (default 18)
 TARGET_TEMP="55" # Set target CPU temperature (default 55)
-POLL_INT="5" # Polling time in seconds (default 5)
+POLL_INT="15" # Polling time in seconds (default 5)
+
+# Colors for terminal
+if test -t 1; then
+    ncolors=$(which tput > /dev/null && tput colors)
+    if test -n "$ncolors" && test $ncolors -ge 8; then
+        normal="$(tput sgr0)"
+        red="$(tput setaf 1)"
+        green="$(tput setaf 2)"
+    fi
+fi
 
 # Setting up GPIO pin
 if [ ! -e /sys/class/gpio/gpio"$GPIO_N" ]; then
@@ -23,12 +33,14 @@ fi
 
 setCoolerOn()
 {
-  sudo echo "1" > /sys/class/gpio/gpio"$GPIO_N"/value
+    # Turn cooler on
+    echo "1" > /sys/class/gpio/gpio"$GPIO_N"/value
 }
 
 setCoolerOff()
 {
-  sudo echo "0" > /sys/class/gpio/gpio"$GPIO_N"/value
+    # Turn cooler off
+    echo "0" > /sys/class/gpio/gpio"$GPIO_N"/value
 }
 
 getTemp()
@@ -51,24 +63,24 @@ do
   getTemp
   if [ "$CPU_TEMP" -gt "$TARGET_TEMP" ]; then
     if [ "$isCooling" == "yes" ]; then
-      # Uncomment line below to debug
-      # echo "CPU temperature is too high: $CPU_TEMP, keep cooling..."
+     # Uncomment line below to debug
+     # echo "CPU temperature is too high: ${red}$CPU_TEMP${normal}, keep cooling..."
+     isCooling="yes"
     else
-      # Uncomment line below to debug
-      echo "CPU temperature is too high: $CPU_TEMP, start cooling..."
-      setCoolerOn
-      isCooling="yes"
+     echo "CPU temperature is too high: ${red}$CPU_TEMP${normal}, start cooling..."
+     setCoolerOn
+     isCooling="yes"
     fi
   else
     if [ "$isCooling" == "yes" ]; then
-      echo "CPU temperature is fine: $CPU_TEMP, cooler stopped."
+      echo "CPU temperature is fine: ${green}$CPU_TEMP${normal}, cooler stopped."
       setCoolerOff
       isCooling="no"
     else
       # Uncomment line below to debug
-      # echo "CPU temperature is fine: $CPU_TEMP, no need to cool."
+      # echo "CPU temperature is fine: ${green}$CPU_TEMP${normal}, no need to cool."
+      isCooling="no"
     fi
   fi
-  # Next update after POLL_INT seconds
   sleep "$POLL_INT"
 done
